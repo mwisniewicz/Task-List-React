2c8ed39aa27255b3bbb9944cbf41c0f711755272
Mateusz Wisniewicz <m.wisniewicz@gmail.com>
2017-12-07 23:54:35 +0100

    removed some stuff

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-import './utils.js'
-import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-console.log('webpack started');
-
-console.log(validator.isEmail('m.wisniewiczgmail.com'));
 
 const template = <p>Tested JSX</p>;
 ReactDOM.render(template,document.getElementById('app'));
